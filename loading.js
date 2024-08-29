f75bd54b5e4b14effd758af1d30fc817cd00c6d3
CarlosCortesEscobar <ccortesescobar19@mail.wou.edu>
2024-08-28 21:46:35 -0500

    site rework v1

--- loading.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loading = document.getElementsByClassName('loading')[0]
-    const loaded = document.getElementsByClassName('loaded')[0]
-    window.addEventListener('load', () => {
-        loading.style.display = "none"
-        loaded.style.display = "flex"
-    })
-})
